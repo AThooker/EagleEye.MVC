ce38a14336efda22341f64ab62a802dc70118079
AThooker <austintrent@icloud.com>
2020-08-20 18:07:06 -0400

    mvc scaffold, 1 data class with inheritance

---
+++ EagleEye.Data/Incident.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EagleEye.Data
+{
+    public class Incident : Site
+    {
+        public int IncidentID { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Phone { get; set; }
+        [ForeignKey(nameof(VictimId))]
+        public virtual Victim Victim { get; set; }
+        public int VictimId { get; set; }
+        [ForeignKey(nameof(PerpId))]
+        public virtual Perp Perp { get; set; }
+        public DateTimeOffset TimeOfIncident { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+
+    }
+}
---
+++ EagleEye.Data/Site.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EagleEye.Data
+{
+    public abstract class Site
+    {
+        [Key]
+        public int SiteID { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
